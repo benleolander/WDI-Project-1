e352319a781259ac6529e214a9773983111c4ef5
Ben Lander <ben.l.lander@googlemail.com>
2019-01-05 16:35:31 +0000

    Added player movement & boundaries

--- js/app.js
+++ js/app.js
@@ -1,15 +1,46 @@
-$(() => {
-  const width = 20
-  const $grid = $('.grid')
-  let $squares
+const width = 20
+let $grid
+let $squares
+let playerPosition = (width*width) - (width/2)
 
+function moveLeft() {
+  if(playerPosition > (width*width)-width) {
+    $squares.removeClass('player')
+    playerPosition--
+    $squares.eq(playerPosition).addClass('player')
+  }
+}
+
+function moveRight() {
+  if(playerPosition < (width*width)-1) {
+    $squares.removeClass('player')
+    playerPosition++
+    $squares.eq(playerPosition).addClass('player')
+  }
+}
+
+function handleKeydown(e) {
+  console.log('Keydown call')
+  switch(e.keyCode) {
+    case 37: moveLeft()
+      break
+    case 39: moveRight()
+      break
+    // case 32: shoot
+  }
+}
+
+function init() {
+  $grid = $('.grid')
   for (let i=0; i<width*width; i++) {
     $grid.append($('<div />'))
   }
   $squares = $grid.find('div')
 
-  const playerStart = $squares.eq((width*width)-(width/2))
+  $squares.eq(playerPosition).addClass('player')
 
-  playerStart.addClass('player')
+  $(document).on('keydown', handleKeydown)
+  console.log('Init complete')
+}
 
-})
+$(init)
